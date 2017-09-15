22a61e8a9b7bb8f68e35fd669fa1b4a57df48665
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2017-09-15 12:01:36 -0700

    Create navbar.php

---
+++ src/navbar.php
@@ -0,0 +1,35 @@
+
+<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        				<span class="icon-bar"></span>
+        				<span class="icon-bar"></span>
+        				<span class="icon-bar"></span> 
+      				</button>
+				<a class="navbar-brand" href="/">Nanopi</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li class="active">
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/games">Games</a>
+					</li>
+					<li>
+						<a href="/tools">Tools</a>
+					</li>
+       					<li>
+						<a href="/chat">Chat</a>
+					</li>
+					<li>
+						<a href="https://music.nanopi.ml/">Music</a>
+					</li>
+						<li>
+							<a href="/login">Login</a>
+						</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
