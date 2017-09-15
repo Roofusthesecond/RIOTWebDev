71809f9852b8fed923f83d1616927e1c12abfb0a
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2017-09-15 11:59:42 -0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -9,40 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
   </head>
   <body>
-    <nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        				<span class="icon-bar"></span>
-        				<span class="icon-bar"></span>
-        				<span class="icon-bar"></span> 
-      				</button>
-				<a class="navbar-brand" href="/">Nanopi</a>
-			</div>
-			<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav">
-					<li class="active">
-						<a href="/">Home</a>
-					</li>
-					<li>
-						<a href="/games">Games</a>
-					</li>
-					<li>
-						<a href="/tools">Tools</a>
-					</li>
-       					<li>
-						<a href="/chat">Chat</a>
-					</li>
-					<li>
-						<a href="https://music.nanopi.ml/">Music</a>
-					</li>
-						<li>
-							<a href="/login">Login</a>
-						</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+    <?php include'navbar.php';?>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
