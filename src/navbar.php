b54cc6e1f0e7997a1db4bba3bafad30c86a514aa
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2017-09-25 11:20:44 -0700

    PHP win test script

--- src/navbar.php
+++ src/navbar.php
@@ -1,31 +1,5 @@
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<nav class="navbar navbar-dark bg-dark">
   
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">RIOT</a>
 
- <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active" id="Home">
-        <a class="nav-link" href="#">Home <span class="sr-only"></a>
-      </li>
-      <li class="nav-item dropdown" id="navbar-Groups" href=""> 
-	      Groups
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="/Web_Development">Web Development</a>
-          <a class="dropdown-item" href="/Robotics">Robotics</a>
-          <a class="dropdown-item" href="/Programming">Programming</a>
-	  <a class="dropdown-item" href="/Cyber_Security">Cyber Security</a>
-   	  <a class="dropdown-item" href="/Management_and_Development">Management and Development</a>
-        </div>    
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/About">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/Donations">Donations</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/Contracts">Contracts</a>
-      </li>
-    </ul>
-  </div>
 </nav>
