b3b3953b119b8b36d9947f3aac36ba4e7586ba50
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2017-09-25 11:44:32 -0700

    navbar work

--- src/navbar.php
+++ src/navbar.php
@@ -1,5 +1,16 @@
 <nav class="navbar navbar-dark bg-dark">
-  
-  <a class="navbar-brand" href="#">RIOT</a>
-
+	<a class="navbar-brand" href="#">RIOT</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbar-items" aria-controls="navbar-items" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbar-items">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="/about">About</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/about">About</a>
+			</li>
+		</ul>
+	</div>
 </nav>
