d8c04a28242e2e5cbed1758011103386c61d9371
adumbidiot <nathaniel.daniel23@outlook.com>
2017-09-30 14:08:48 -0700

    General fixes
    About page creation, navbar work, robots.txt creation, php dir cleanup

--- src/navbar.php
+++ src/navbar.php
@@ -3,11 +3,17 @@
   		display: block;
 	}
 </style>-->
-
-
+<style>
+	.dropdown-item{
+		font-weight: 300;
+	}
+	.nav-link{
+		font-weight: 300;	
+	}
+</style>
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
 	<a class="navbar-brand" href="/">
-		<img src="./apple-icon.png" style="width:25px;" />	
+		<img src="/apple-icon.png" style="width:20px;" />	
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
