c0eb136a84b26134789883af54bb69834a6e611f
adumbidiot <nathaniel.daniel23@outlook.com>
2017-09-29 23:37:13 -0700

    Navbar work

--- src/navbar.php
+++ src/navbar.php
@@ -6,7 +6,9 @@
 
 
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-	<a class="navbar-brand" href="/">RIOT</a>
+	<a class="navbar-brand" href="/">
+		<img src="./apple-icon.png" style="width:25px;" />	
+	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
@@ -18,11 +20,11 @@
    					 Groups
  			    	</a>
  			 	<div class="dropdown-menu">
-				    <a class="dropdown-item" href="/groups/index.php#Web-Development">Web Development</a>
-				    <a class="dropdown-item" href="/groups/index.php#Robotics">Robotics</a>
-				    <a class="dropdown-item" href="/groups/index.php#Cyber-Security">Cyber Security</a>
-				    <a class="dropdown-item" href="/groups/index.php#Programming">Programming</a>
-				    <a class="dropdown-item" href="/groups/index.php#Management-and-Development">Management and Development</a>
+				    <a class="dropdown-item" href="/groups/#Web-Development">Web Development</a>
+				    <a class="dropdown-item" href="/groups/#Robotics">Robotics</a>
+				    <a class="dropdown-item" href="/groups/#Cyber-Security">Cyber Security</a>
+				    <a class="dropdown-item" href="/groups/#Programming">Programming</a>
+				    <a class="dropdown-item" href="/groups/#Management-and-Development">Management and Development</a>
   				</div>
 			</div>
 			<li class="nav-item">
