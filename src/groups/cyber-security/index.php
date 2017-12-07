fc9038904fd3b5affe8a499bae5e014fce17e5cb
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2017-12-07 11:45:02 -0800

    Individual group pages fixed

--- src/groups/cyber-security/index.php
+++ src/groups/cyber-security/index.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Web Development</title>
+    <title>Cyber Security</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-  		<style type="text/css">
-  			.group-div{
-				border: 2px solid #666;	
-				border-radius: 10px;
-				height: 500px;
-			} 
-			.trello-groups {
-				height: 100%;
-				width: 100%;
-			} 
-  		</style>
+      <style type="text/css">
+        .group-div{
+        border: 2px solid #666; 
+        border-radius: 10px;
+        height: 500px;
+      } 
+      .trello-groups {
+        height: 100%;
+        width: 100%;
+      } 
+      </style>
   </head>
   <body>
     <?php include'../../navbar.php';?>
-    <div></div>
-		<div id="Web-Development" class="col-md-12 group-div inside-div-trello">
-			<div id="trello-web-dev"  class="trello-groups">
-				<iframe class="trello-groups" frameborder="0" src="https://trello.com/b/IQOthQcV.html"></iframe>
-			</div>
+    <div>
+      <div id="Cyber-Security" class="col-md-12 group-div" class="trello-groups inside-div-trello">
+		<div id="trello-cyber-security" class="trello-groups">
+			<iframe frameborder="0" src="https://trello.com/b/LcnXMWCh.html" class="trello-groups"></iframe>
 		</div>
+	  </div>
 
     </div>
     
