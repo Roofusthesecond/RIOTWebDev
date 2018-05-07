05d630438ab0e97c2b7af69d84a3a1f6717f29e8
adumbidiot <nathaniel.daniel23@outlook.com>
2018-05-06 19:43:34 -0700

    Threw in a bunch of easter eggs

--- src/groups/web-development/index.php
+++ src/groups/web-development/index.php
@@ -21,10 +21,14 @@
   <body>
     <?php include'../../navbar.php';?>
     <div class="container-fluid text-center">
-		<h1 style="font-weight: 200; padding: 100px;">Web Development</h1>
-		<div id="Web-Development" class="col-md-12 group-div inside-div-trello">
-			<div id="trello-web-dev"  class="trello-groups">
-				<iframe class="trello-groups" frameborder="0" src="https://trello.com/b/VqeyyqrX.html"></iframe>
+		<div class="row">
+			<div class="col-md-12">
+				<h1 style="font-weight: 200; padding: 30px;">Web Development</h1>
+			</div>
+			<div id="Web-Development" class="col-md-12 group-div inside-div-trello">
+				<div id="trello-web-dev"  class="trello-groups">
+					<iframe class="trello-groups" frameborder="0" src="https://trello.com/b/VqeyyqrX.html"></iframe>
+				</div>
 			</div>
 		</div>
     </div>
@@ -33,6 +37,47 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	<script src="/js/egg.js"></script>
+	<script src="/easter-eggs/boner.js" async></script>
+	<script src="/easter-eggs/libdoge.js" async></script>
+	<script>
+		let inner = document.getElementById('Web-Development');
+		
+		function spawnYT(){
+			inner.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/H3oiThw2RxE?autoplay=1" frameborder="0" align="center" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+		}
+		
+		let b = new Egg('boner', function(){
+			console.log('Boi');
+			$("body").boner({
+				bSize : 20,
+				bSpeed : 30
+			});	
+		});
+		let t = new Egg('thanks', function(){
+			console.log('Thanks');
+			inner.innerHTML = '<embed src="/easter-eggs/thankstanks.swf" width="600" height="350"></embed>';
+		});
+		let d = new Egg('doge', function(){
+			console.log('Very anger');
+			controller.buyDoge();
+		}, {continue: true});
+		let dc = new Egg('money', function(){
+			console.log('The superior currency has arrived!');
+			spawnYT();
+		});
+		let beauty = new Egg('beauty', function(){
+			console.log('POWER');
+			for(let i = 0; i != 5; i++){
+				controller.buyDoge();
+			}
+			spawnYT();
+		});
+		let nick = new Egg('nick', function(){
+			console.log('NOT THE BEES');
+			document.head.innerHTML += '<style>*{background-image: url("/img/nick.jpg")}</style>';
+		});
+	</script>
   </body>
 </html>
 
