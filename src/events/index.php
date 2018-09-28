c010223a9f597e1ffa98e11110ef98f34b74b79a
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2018-09-28 11:21:36 -0700

    Pics + cleanup

---
+++ src/events/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Events</title>
+			<meta charset="utf-8">
+    		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+	<?php include'navbar.php';?>
+
+	<div class="text-center container">
+		<iframe src="https://calendar.google.com/calendar/embed?src=rocklinusd.org_7urp5rm04s95lpo4qjci83bb2s%40group.calendar.google.com&ctz=America%2FLos_Angeles"/>
+	</div>
+</body>
+</html>
